6c7d7345f60d29d8b7fd44e11f248456774bbed5
JoelV00 <letteruser10@gmail.com>
2024-07-25 12:46:10 +0530

    Second

--- GitCmd.java
+++ GitCmd.java
@@ -1,7 +1,11 @@
 package nishathi;
 
 public class GitCmd {
+	public static int add(int a,int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
+		System.out.println(add(5,100));
 		
 	}
 
