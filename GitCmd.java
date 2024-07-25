9777d8f310f2d734ef9dd370e3a12c760bd5c387
JoelV00 <letteruser10@gmail.com>
2024-07-25 13:02:32 +0530

    third commit

--- GitCmd.java
+++ GitCmd.java
@@ -1,8 +1,11 @@
 package nishathi;
 
 public class GitCmd {
+	public static boolean ret(boolean a) {
+		return a;
+	}
 	public static void main(String[] args) {
-		
+		System.out.println(ret(true));
 	}
 
 }
