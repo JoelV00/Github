3e5d190b8325618f95d9a6f5ca44912b2a972fe4
santhosh kumar <91813816+CAGER-YT@users.noreply.github.com>
2024-07-25 12:56:10 +0530

    Update GitCmd.java

--- GitCmd.java
+++ GitCmd.java
@@ -2,6 +2,7 @@ package nishathi;
 
 public class GitCmd {
 	public static void main(String[] args) {
+		//String test
 		String a="joel";
 		System.out.println(a+" vasan");
 		System.out.println("Test Sucess");
